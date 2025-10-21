113a0523a4b709eb98f116e7fed6db8efe72e713
SlimeKlone <slimeklone@gmail.com>
2025-10-21 10:51:09 +1000

    Boom

--- QUBE/Assets/Script/Rotator.cs
+++ QUBE/Assets/Script/Rotator.cs
@@ -4,7 +4,11 @@ public class Rotator : MonoBehaviour
 {
     public float RotationSpeed = 50f;
     public CoinCounter coinCounter;
-
+    public GameObject Boom;
+    void Start()
+    {
+    
+}
     void Update()
     {
         transform.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
@@ -12,9 +16,11 @@ public class Rotator : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
+        
         // Проверяем, что это игрок
         if (other.CompareTag("Player"))
         {
+            Instantiate(Boom,  gameObject.transform.position,gameObject.transform.rotation);
             // Уничтожаем текущий объект
             coinCounter.AddCoin();
             Destroy(gameObject);
